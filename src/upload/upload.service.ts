3aaa9fb2be3798fe8559e66870b645af96c5bf95
LeeChangGeun <echk1212@gmail.com>
2024-06-18 19:24:40 +0900

    Add gitgub actions

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -10,7 +10,7 @@ export class UploadService {
     this.s3 = new AWS.S3({
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
-      region: process.env.AWS_REGION,
+      region: process.env.AWS_DEFAULT_REGION,
     });
   }
 
